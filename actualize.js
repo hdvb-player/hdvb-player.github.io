fd055d10cc9313b170de3734582d1876d617cb2b
hdvb-player <95034623+hdvb-player@users.noreply.github.com>
2024-05-29 11:08:51 +0300

    Update actualize.js

--- actualize.js
+++ actualize.js
@@ -1,47 +1,101 @@
-var s, d, ss, gs, hm, l, n = "https://vid" + Date.now() + ".fotpro135alto.com";
-l = function(t, e) {       
-    document.body && Array.prototype.find.call(document.body.getElementsByTagName("iframe"), function(a) {
-        Array.prototype.find.call(t, function(o) {
-            var i = new RegExp(o),
-                r = a.src.match(i);
-            if (null !== r && r.length > 0) {
-                var s = a.src.replace(r[0], n);
-                e(a, o, s, t)
-            }
-        })
-    })
-}, 
-s = function(t) {
-    var e, a;
-    l(t, function(n, t, o, i) {
-        e = n, a = o
-    }), "contentWindow" in e && (ss = function(n, t) {
-        "localStorage" in window && localStorage.setItem(n, JSON.stringify(t))
-    }, gs = function(n) {
-        return "localStorage" in window && JSON.parse(localStorage.getItem(n))
-    }, a = (d = function(n) {
-        let t = new RegExp(/\/(?:movie|serial)\/(.*)(?:\/iframe)/);
-        if (t.test(n)) return n.match(t)
-    })(a), e.onload = function() {
-        e.contentWindow.postMessage({event: 'timestore_localstorage', info: gs(a[1])}, "*")
-    }, hm = function(t) {
-        if ("data" in t && "object" == typeof t.data) {
-                if (t.data.event == "timestore_localstorage") {
-            "title" in t.data.info && "value" in t.data.info && ss(a[1], t.data.info)
-                }
-        }
-    }, "addEventListener" in window ? window.addEventListener("message", hm) : window.attachEvent("message", hm))
+!function(){
+    var conf=window['__hdvb.js'];
+    if(conf)return;window['__hdvb.js']=conf={deploy:0.1};
+    var actual = "https://kinolordfilm.com"
+        ,re=/^(?:https?:)?\/\/(?:vid?\w*)\.(makrohd\.(info|com|net)|hdvb\.(cc|xyz)|videolishd\.(net|com)|farsihd\.(info|pw)|tehranvd\.(ru|su|net|com|org|pw|biz)|my-serials\.info|(vb17102bernardjordan|vb17100astridcoleman|vb17101tesswalton|vb17103elmerwatson|vb17104alfredcurry|vb17105quinnnash|vb17106cecilgregory|vb17107rexhammond|vb17108janenoble|vb17109nadiapattel|vb17110frankfoley|vb17111milesbowman|vb17112tiffanyhaywardvb17120ayeshajenkins|vb17121coramclean|vb17123filippaaniketos|vb24130jarvisletos)(\.pw)|(vb24131crasosnemesis|vb24132nightdwellers|azure133sitsarl|clement134quo|fotpro135alto|kinolordfilm|ccdnbst)(\.com))/
 
-},
-function() {
-    var n, t = [];
-    n = function(n) {
-        t.push("^https?:\\/\\/(vii?dd?\\d*.)?{DOMAIN}".replace("{DOMAIN}", n))
-    }, ["makrohd.(info|com|net)", "hdvb.(cc|xyz)", "videolishd.(net|com)", "farsihd.(info|pw)", "tehranvd.(ru|su|net|com|org|pw|biz)", "my-serials.(info)", "vb17102bernardjordan.(pw)", "vb17100astridcoleman.(pw)", "vb17101tesswalton.(pw)", "vb17103elmerwatson.(pw)", "vb17104alfredcurry.(pw)", "vb17105quinnnash.(pw)", "vb17106cecilgregory.(pw)", "vb17107rexhammond.(pw)", "vb17108janenoble.(pw)", "vb17109nadiapattel.(pw)", "vb17110frankfoley.(pw)", "vb17111milesbowman.(pw)", "vb17112tiffanyhayward.(pw)", "vb17120ayeshajenkins.(pw)", "vb17121coramclean.(pw)", "vb17123filippaaniketos.(pw)", "vb24130jarvisletos.(pw)", "(vb24131crasosnemesis|vb24132nightdwellers|azure133sitsarl|clement134quo|fotpro135alto).(com)"].forEach(function(t, e) {
-        n(t)
-    }), l(t, function(n, t, e, a) {
-        n.setAttribute("src", ""), setTimeout(function() {
-            n.setAttribute("src", e), s(a)
-        }, 100)
-    })
-}();
+        ,delay=200,max=1000*60*60*24
+        ,dry=[]
+        ,ua=navigator.userAgent
+        ,ios=~ua.indexOf('iPhone')
+        ,ral
+        ,m = ua.match(/Chrome\/(\d+)\./),v = m && +m[1] || 1
+        ,IM = ' !important;',css=document.createElement('style');
+    css.innerHTML= '.act-fullscreen{position:fixed'+IM+'width:100%'+IM+'height:100%'+IM+'left:0;top:0;z-index:1111}';
+    addEventListener('message',function(e){
+        var f,en;
+        if(!e.data||!(f=findFrame(function (f){return f.contentWindow==e.source})))return;
+        if(e.data.event=='externalFullScreen?'){
+            if(!css.parentNode)document.head.appendChild(css);
+            e.source.postMessage({event:'externalFullScreen!'});
+        }else if(e.data.event=='toggleFullScreen'){
+            en=f.classList.toggle('act-fullscreen');
+            e.source.postMessage({event:'fullScreen',enter:en});
+        }
+    });
+    replace();
+    function replace(){
+        if(delay<max)delay++;
+        setTimeout(replace,delay);
+        var old,src,ds,i=findFrame(function(f){
+            src=f.src;
+            if(!src&&(ds=f.dataset)){
+                if(ds.src){
+                    old=ds.src.match(re);
+                    if(old)ds.src=ds.src.replace(old[0],'https://vid'+Date.now()+'.'+old[1])
+                }if(/\blazyload(ed|ing)\b/.test(f.className))src=ds.src;
+            }
+
+            return old=src&&src.indexOf(actual)&&dry.indexOf(src)===-1&&src.match(re)
+        });
+
+        if (!i ||i.offsetWidth===0) return;
+        dry.push(src);
+        var url = src.replace(old[0], actual);
+        get(url, function(r){
+            if(ral===1)return;
+            var up=update(i,r);
+            dry=[];
+            // if(window.URL){
+            //  url=new URL(url);
+            //  url.searchParams.delete('episode');
+            // }
+            addEventListener('message',function(e){
+                if(e.origin==location.origin&&e.data=='reActualizeMe'&&up.contentWindow==e.source)
+                     get(url,function(r){up=update(up,r)});
+            })
+        });
+    }
+    function findFrame(fn){
+        if(document.body)return Array.prototype.find.call(document.body.getElementsByTagName('iframe'),fn);
+    }
+    function update(old,w){
+        var up = document.createElement('iframe'),
+            allow = 'autoplay *; fullscreen';
+        if (ios || /mac os/i.test(navigator.userAgent)) allow += ' *';
+        copyAttr(old,up);
+        up.setAttribute('allow', allow);
+        up.allow = allow;
+        old.parentElement.replaceChild(up,old);
+        up.contentDocument.write(w);
+        up.contentDocument.close();
+
+        var s=up.setAttribute;
+        up.setAttribute = function(n,v){
+            if(n=='src'&&!v) console.warn('empty src')
+            else s.call(up,n,v)
+        };
+        return up;
+    }
+    function copyAttr(from,to){
+        var attrs=from.attributes;
+        for(var name,i=0;i<attrs.length;i++){
+            name = attrs[i].name;
+            if(name!='src'&&name!='data-src'&&/^[\w\-]+$/.test(name))
+                to.setAttribute(name,attrs[i].value);
+        }
+    }
+    function get(url, cb) {
+        var xhr = new XMLHttpRequest;
+        // xhr.withCredentials = true;
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) cb(xhr.response);
+        };
+        xhr.send();
+
+    }
+    function head(u) {
+        return fetch(u,{method:'head'});
+    }
+}()
